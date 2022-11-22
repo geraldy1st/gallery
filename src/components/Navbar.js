fcf899c6f3ac30d040edfb7c7d492080010777a5
geraldy1st <geraldy.leondas@gmail.com>
2022-11-22 15:20:07 +0100

    bug fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar bg-teal-700 p-3 text-white">
-      <a href="/">Gallery</a>
+      <a href="/gallery">Gallery</a>
     </nav>
   );
 };
