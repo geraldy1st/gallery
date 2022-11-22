fa408d769901beac1e00d598831ec2ef2bcf908a
geraldy1st <geraldy.leondas@gmail.com>
2022-11-22 15:18:42 +0100

    bug fixed

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -20,7 +20,7 @@ const ImageSearch = ({ searchText }) => {
             placeholder="Search Image Term..."
           />
           <button
-            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700  text-sm border-4 hover:text-white py-1 px-2 rounded"
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
             type="submit"
           >
             Search
